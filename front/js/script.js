ae7c709df79e52632fc5c1d016d7a6729bffca78
gunelalasgarli <gunelsha@code.edu.az>
2021-12-02 20:56:20 +0400

    homepage is about to be done

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
     $('.slick-list').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -12,11 +14,39 @@ $(document).ready(function() {
         arrows: true,
         nextArrow: `<i class="fal fa-chevron-right iconright slick-next"></i>`,
         prevArrow: `<i class="fal fa-chevron-left iconleft slick-prev"></i>`,
-        respondTo: 'window',
-
-
+        respondTo: 'window'
     });
 
+    $(window).scroll(function(e) {
+        let position = $(this).scrollTop();
+        if (position > 400) {
+            document.getElementById("navmenu").classList.add("stickynavbar");
+        } else {
+            document.getElementById("navmenu").classList.remove("stickynavbar");
+        }
+        if (position > 100) {
+            document.getElementById("cart").innerHTML = ' <a href=""><img src="img/icons/cart.png" alt=""> <p> Səbət </p> </a> ';
+        } else {
+            document.getElementById("cart").innerHTML = '';
+        }
+        if (position > 100) {
+            document.getElementById("domino").innerHTML = ' <a href=""><img class="domino" src="img/icons/single_domino.png" alt=""></a>';
+        } else {
+            document.getElementById("domino").innerHTML = '';
+        }
+    });
+    // let loader = document.getElementById("loader");
+    // window.addEventListener("load", function() {
 
+    //     loader.style.visibilty = "visible";
 
+    // })
+    function hideLoader() {
+        $('#loader').hide();
+    }
+
+    $(window).ready(hideLoader);
+
+    // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+    setTimeout(hideLoader, 20 * 1000);
 });
