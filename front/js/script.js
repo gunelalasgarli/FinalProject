cd5709e58460c5e36e999344d087044e87eca642
gunelalasgarli <gunelsha@code.edu.az>
2021-12-12 18:21:17 +0400

    Pizza page scrollspy problem solved

--- front/js/script.js
+++ front/js/script.js
@@ -106,35 +106,18 @@ $(document).ready(function() {
 
     var quantitiy = 0;
     $('.quantity-right-plus').click(function(e) {
-
-        // Stop acting like a button
         e.preventDefault();
-        // Get the field name
         var quantity = parseInt($('#quantity').val());
-
-        // If is not undefined
-
         $('#quantity').val(quantity + 1);
-
-
     });
 
 
     $('.quantity-left-minus').click(function(e) {
-        // Stop acting like a button
         e.preventDefault();
-        // Get the field name
         var quantity = parseInt($('#quantity').val());
-
-        // If is not undefined
-
-        // Increment
         if (quantity > 0) {
             $('#quantity').val(quantity - 1);
         }
-
-
     });
-
 
 });
